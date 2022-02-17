772a16573a9768496fd0af7bfe66682eae24a8d0
Carl Klagba <carl.klagba@gmail.com>
2022-02-17 15:04:22 +0100

    simple components dynamic creation

---
+++ components/IdeaCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card-text>
+    This is a cool idea !
+  </v-card-text>
+</template>
+
+<script>
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class IdeaCard extends Vue{
+}
+</script>
+
+<style scoped>
+
+</style>
